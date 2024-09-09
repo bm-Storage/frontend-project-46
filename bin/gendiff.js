59dae7b16daf67a9b4539bd3e367377fc3007b2d
Roman Saitov <strathisla.72@gmail.com>
2024-09-10 02:15:14 +0500

    update  package.json package-lock.json bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
-import { program } from "commander";
+import { Command } from "commander";
+import getDiff from "../index.js";
 
+const program = new Command();
 
-export default program
+program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference')
   .version('1.0.0')
-  .argument('<filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+
+    console.log(getDiff(filepath1, filepath2));
+
+});
 
 program.parse(process.argv);
-
